e0ac95bf89f2b27b424ef47461811db06769d50a
JamesHare <harejamesm@gmail.com>
2020-04-21 21:08:21 -0500

    BOXOM-1001 Added Spring Initializr base

---
+++ src/main/java/com/jamesmhare/boxofficemanager/BoxOfficeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jamesmhare.boxofficemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxOfficeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxOfficeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamesmhare/boxofficemanager/BoxOfficeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesmhare.boxofficemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxOfficeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
